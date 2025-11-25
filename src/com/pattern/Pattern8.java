cbd70774f9e66fd7b04806545a2355ebdb332440
unknown <vishalkumarsinghv60@gmail.com>
2025-11-25 18:13:49 +0530

    second commit

--- src/com/pattern/Pattern8.java
+++ src/com/pattern/Pattern8.java
@@ -2,23 +2,20 @@ package com.pattern;
 
 import java.util.Scanner;
 
-public class Pattern7 {
+public class Pattern8 {
 
 	public static void main(String[] args) {
 		System.out.println("Enter the number of rows");
 		Scanner sc = new Scanner(System.in);
 		int rows = sc.nextInt();
 		for (int i = 1; i <= rows; i++) {
-			for (int j = 1; j <= rows - i; j++) {
+			for (int j = 1; j < i; j++) {
 				System.out.print(" ");
 			}
-			for (int j = 1; j <= 2 * i - 1; j++) {
+			for (int j = 1; j <= 2 * rows - (2 * i + 1); j++) {
 				System.out.print("*");
 			}
 			System.out.println();
-//			for (int j = 1; j <= rows - i; j++) {
-//				System.out.print("");
-//			}
 		}
 
 	}
