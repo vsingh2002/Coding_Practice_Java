9ff5390839bd5aa891e2063894ae93ffc112bce9
unknown <vishalkumarsinghv60@gmail.com>
2025-12-01 13:03:14 +0530

    fourth commit

---
+++ src/com/pattern/Pattern10.java
@@ -0,0 +1,24 @@
+package com.pattern;
+
+import java.util.Scanner;
+
+public class Pattern10 {
+	public static void main(String[] args) {
+		System.out.println("Enter the number of rows");
+		Scanner sc = new Scanner(System.in);
+		int rows = sc.nextInt();
+		for (int i = 1; i <= rows; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int i =1; i<=rows; i++) {
+			for (int j = 1; j < rows - i + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
