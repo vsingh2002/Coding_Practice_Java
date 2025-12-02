e7b7e1160ffb64a9729a27b280aef3bcc0c411fd
unknown <vishalkumarsinghv60@gmail.com>
2025-12-02 11:06:46 +0530

    sixth commit

--- src/com/pattern/Pattern12.java
+++ src/com/pattern/Pattern12.java
@@ -2,22 +2,27 @@ package com.pattern;
 
 import java.util.Scanner;
 
-public class Pattern11 {
+public class Pattern12 {
 	public static void main(String[] args) {
 		System.out.println("Enter the number of rows");
 		Scanner sc = new Scanner(System.in);
 		int rows = sc.nextInt();
 		for (int i = 1; i <= rows; i++) {
 			for (int j = 1; j <= i; j++) {
-				if ((i + j) % 2 == 0) {
-					System.out.print("1 ");
-				} else {
-					System.out.print("0 ");
-				}
+
+				System.out.print(j);
+			}
+
+			for (int j = 1; j <= 2 * (rows - i); j++) {
+				System.out.print(" ");
+			}
+
+			for (int j = i; j >= 1; j--) {
+				System.out.print(j);
 			}
 			System.out.println();
-
 		}
+
 	}
 
 }
