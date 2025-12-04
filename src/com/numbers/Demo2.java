37a9f1266f9b6e3b553843bb608b863c957cf372
unknown <vishalkumarsinghv60@gmail.com>
2025-12-04 16:02:55 +0530

    reverse a number

--- src/com/numbers/Demo2.java
+++ src/com/numbers/Demo2.java
@@ -2,21 +2,19 @@ package com.numbers;
 
 import java.util.Scanner;
 
-// Count Number digits
-public class Demo1 {
+// Reverse a number
+public class Demo2 {
 	public static void main(String[] args) {
 		System.out.println("Enter the number");
-
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-		int count = 0;
+		int rev = 0;
 		while (num > 0) {
-			//int d = num % 10;
-			count++;
+			int d = num % 10;
+			rev = rev * 10 + d;
 			num = num / 10;
 		}
-
-		System.out.println("Number of digits is :" + count);
+		System.out.println("Reverse of a number is :" + rev);
 		sc.close();
 	}
 
