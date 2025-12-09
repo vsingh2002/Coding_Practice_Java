4f4aa530ef9be3823fe7ad84f4f5295b83bf5928
unknown <vishalkumarsinghv60@gmail.com>
2025-12-09 12:55:02 +0530

    check number is prime or not

---
+++ src/com/numbers/Demo5.java
@@ -0,0 +1,26 @@
+package com.numbers;
+
+import java.util.Scanner;
+
+// Check number is prime or not 
+public class Demo5 {
+	public static void main(String[] args) {
+		System.out.println("Enter the number");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int count = 0;
+		for (int i = 1; i < num; i++) {
+			if (num % i == 0) {
+				count++;
+			}
+		}
+		if (count == 2) {
+			System.out.println("Number is prime");
+
+		} else {
+			System.out.println("Number is not prime");
+		}
+		sc.close();
+	}
+
+}
