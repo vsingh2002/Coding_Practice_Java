55f18d6d82aec5a2c56f621176c4bb71fb797888
unknown <vishalkumarsinghv60@gmail.com>
2025-12-09 13:03:17 +0530

    make changes in the loop condition

--- src/com/numbers/Demo5.java
+++ src/com/numbers/Demo5.java
@@ -9,7 +9,7 @@ public class Demo5 {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
 		int count = 0;
-		for (int i = 1; i < num; i++) {
+		for (int i = 1; i <=num; i++) {
 			if (num % i == 0) {
 				count++;
 			}
